f75ff85f65b295247fe4d9d790d70840bd5aca0e
Lance <me_lance@msn.com>
2021-06-19 14:32:29 -0500

    Removing Definitions project and updating the product name for UITerminal

--- Randomizer.Generator.UITerminal/AssemblyInfo.g.cs
+++ Randomizer.Generator.UITerminal/AssemblyInfo.g.cs
@@ -4,8 +4,8 @@ namespace Randomizer.Generator.UITerminal
 {
     public static partial class AssemblyInfo
     {
-        public static DateTime CompilationTimestampUtc { get { return new DateTime(637597244004490917L, DateTimeKind.Utc); } }
-        public static String ProductName { get => "Randomizer TUI"; }
+        public static DateTime CompilationTimestampUtc { get { return new DateTime(637597278206445244L, DateTimeKind.Utc); } }
+        public static String ProductName { get => "Randomizer UIT"; }
         public static String Author { get => "Lance Boudreaux"; }
       
 #if ALPHA
