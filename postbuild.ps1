3ab14547fe45038f6d692b954298e997e5f36284
Lance <me_lance@msn.com>
2021-06-25 13:00:23 -0500

    Added post build script

---
+++ postbuild.ps1
@@ -0,0 +1,9 @@
+﻿Write-Host("Randomizer.Generator Post Build");
+Write-Host("Copying Randomizer.Generator.CmdLine");
+If (!(Test-Path .\bin\Debug\net5.0))
+{
+    New-Item -Path .\bin\Debug\net5.0
+}
+Copy-Item -Path ..\Randomizer.Generator.CmdLine\bin\Debug\net5.0\* -Destination .\bin\Debug\net5.0 -Recurse -Force
+Write-Host("Copying Randomizer.Generator.UITermincal");
+Copy-Item -Path .\bin\Debug\net5.0\* -Destination ..\bin\Debug\net5.0 -Recurse -Force
